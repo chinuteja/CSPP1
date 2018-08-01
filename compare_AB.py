2a88a1be74493553cd5a00a071c59945d57f90d2
chinuteja <41884987+chinuteja@users.noreply.github.com>
2018-08-01 20:46:54 +0530

    module3 compare_AB.py

---
+++ compare_AB.py
@@ -0,0 +1,10 @@
+varA=input("enter an  varA")
+varB=input("enter an  varB")
+if(type(varA)==str and type(varB)==str):
+	print("string involved")
+if(varA>varB):
+    print("bigger")
+if(varA==varB):
+	print("equal")
+if(varA<varB):
+	print("smaller")
