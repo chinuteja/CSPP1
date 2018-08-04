ccf212d0339ac979730908dc000815cbf2e9a364
chinuteja <chinuteja2008@msitprogram.net>
2018-08-04 12:10:02 +0530

    " -> 0 of 7 passed. pylint: 8.57/10 "

---
+++ cspp1-practice/cspp1 assignment/m6/p1/fizz_buzz.py
@@ -0,0 +1,23 @@
+'''
+author = teja
+date = 8/4/2018
+'''
+
+def main():
+    '''
+    Read number from the input, store it in variable num.
+    '''
+    print("enter a number")
+    num = int(input())
+    if num > 0:
+        for i in range(1, num+1, 1):
+            if i%3 == 0 and i%5 == 0:
+                print("FizzBuzz")
+            elif i%5 == 0:
+                print("Buzz")
+            elif i%3 == 0:
+                print("Fizz")
+            else:
+                print(i) 
+if __name__ == "__main__":
+    main()
