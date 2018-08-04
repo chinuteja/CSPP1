21c22357a03558b403737e1aa1c1fa2e2bfd9378
chinuteja <chinuteja2008@msitprogram.net>
2018-08-04 12:00:11 +0530

    " -> 6 of 7 passed. pylint: 9.17/10 "

--- cspp1-practice/cspp1 assignment/m6/p3/digit_product.py
+++ cspp1-practice/cspp1 assignment/m6/p3/digit_product.py
@@ -2,6 +2,7 @@
 author : teja
 date : 8/4/2018
 '''
+
 def main():
     '''
     this function is to multiply the digits in a nubmber.
@@ -9,12 +10,12 @@ def main():
     int_input = int(input())
     x_1 = 1
     if int_input == 0:
-    	print("0")
+        print("0")
     else:
-    	while int_input != 0:
-        	temp = int_input%10
-        	x_1 = x_1*temp
-        	int_input = int_input//10
-    	print(x_1)
+        while int_input != 0:
+            temp = int_input%10
+            x_1 = x_1*temp
+            int_input = int_input//10
+        print(x_1)
 if __name__ == "__main__":
     main()
