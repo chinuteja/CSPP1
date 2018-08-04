f55820d6f1b1225676f624e9422a59083571fdba
chinuteja <chinuteja2008@msitprogram.net>
2018-08-04 11:57:27 +0530

    " -> 6 of 7 passed. pylint: 5.00/10 "

--- cspp1-practice/cspp1 assignment/m6/p3/digit_product.py
+++ cspp1-practice/cspp1 assignment/m6/p3/digit_product.py
@@ -8,10 +8,13 @@ def main():
     '''
     int_input = int(input())
     x_1 = 1
-    while int_input != 0:
-        temp = int_input%10
-        x_1 = x_1*temp
-        int_input = int_input//10
-    print(x_1)
+    if int_input == 0:
+    	print("0")
+    else:
+    	while int_input != 0:
+        	temp = int_input%10
+        	x_1 = x_1*temp
+        	int_input = int_input//10
+    	print(x_1)
 if __name__ == "__main__":
     main()
