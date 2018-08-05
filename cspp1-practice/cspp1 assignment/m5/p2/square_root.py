c22c73d62e8e03d0b65cb43d0af5e4cc411958ef
chinuteja <chinuteja2008@msitprogram.net>
2018-08-05 17:09:26 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1 assignment/m5/p2/square_root.py
+++ cspp1-practice/cspp1 assignment/m5/p2/square_root.py
@@ -11,11 +11,8 @@ def main():
     epsilon = 0.01
     guess = 0.0
     step = 0.1
-    num_guesses = 0
     while abs(guess**2 - square_root) > epsilon and guess <= square_root:
         guess = guess + step
-        num_guesses = num_guesses + 1
-    print("the number of gusses is", num_guesses)
     if abs(guess**2 - square_root) > epsilon:
         print("couldn't find the square root of given number", square_root)
     else:
