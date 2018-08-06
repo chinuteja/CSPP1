6af19ea37d26a2f7848455fa512f8e4cc54989e5
chinuteja <chinuteja2008@msitprogram.net>
2018-08-06 19:16:56 +0530

    odd.py

---
+++ cspp1-practice/cspp1 assignment/m7/Functions - Odd Exercise/Functions - Odd Exercise/odd.py
@@ -0,0 +1,15 @@
+'''
+author : teja
+date : 6/8/2018
+'''
+def odd(x):
+
+    return x%2 !=0
+    
+
+def main():
+    data = input()
+    print(odd(int(data)))
+
+if __name__== "__main__":
+    main()
