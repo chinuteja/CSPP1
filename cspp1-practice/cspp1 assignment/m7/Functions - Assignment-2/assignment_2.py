e2c9c4650675ca5a79c10457e0e0a749dba76299
chinuteja <chinuteja2008@msitprogram.net>
2018-08-06 22:19:25 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/cspp1 assignment/m7/Functions - Assignment-2/assignment_2.py
@@ -0,0 +1,27 @@
+'''
+author : teja
+date : 6/8/2018
+'''
+def paying_debt_off_in_a_year(balance, annual_interest_rate):
+    '''compute the lowest monthly payment '''
+    monthly_interest_rate = annual_interest_rate/12
+    monthly_payment = 0
+    newbalance = balance
+    while newbalance > 0:
+        monthly_payment += 10
+        newbalance = balance
+        month = 1
+        while month <= 12 and newbalance > 0:
+            newbalance -= monthly_payment
+            newbalance += (monthly_interest_rate * newbalance)
+            month += 1
+    ans = "Lowest Payment: " + str(monthly_payment)
+    print(ans)
+def main():
+    ''' Main function'''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    paying_debt_off_in_a_year(data[0], data[1])
+if __name__ == "__main__":
+    main()
