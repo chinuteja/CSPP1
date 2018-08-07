d8bbb8b0dd61d8e3fb73e5aee9400d25a8c30f2e
chinuteja <chinuteja2008@msitprogram.net>
2018-08-07 14:06:08 +0530

    power_iter

---
+++ cspp1-practice/cspp1 assignment/m8/power using Iteration/power_iter.py
@@ -0,0 +1,24 @@
+'''
+author : teja
+date : 7/8/2018
+'''
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    # Your code here
+    y = base**exp
+    return y
+
+
+def main():
+
+    data = input()
+    data = data.split()
+    print(iterPower(float(data[0]),int(data[1]))) 
+
+if __name__== "__main__":
+    main()
