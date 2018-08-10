6f5e81209cfbe10993460ec1436ce64026028816
chinuteja <chinuteja2008@msitprogram.net>
2018-08-10 19:40:53 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-practice/cspp1 assignment/m11/p3/assignment3.py
+++ cspp1-practice/cspp1 assignment/m11/p3/assignment3.py
@@ -15,7 +15,8 @@ def is_valid_word(word_test, hand_word, word_list):
     for i_1 in word_test:
         if i_1 in hand_word:
             count += count
-        if count == len(word_test) and (if word_test[i_1] in word_list) :
+        if count == len(word_test): 
+            if word_test[i_1] in word_list:
             return True
     return False
 def main():
