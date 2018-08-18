20742bbcc1929c4e3ac9675ffb027b3fd98fcbbe
chinuteja <chinuteja2008@msitprogram.net>
2018-08-18 10:59:22 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- cspp1-practice/m3/compare_AB.py
+++ cspp1-practice/m3/compare_AB.py
@@ -5,7 +5,7 @@ date : 1/8/2018
 
 VARA = input("enter an  varA")
 VARB = input("enter an  varB")
-if(type(VARA)==str and type(VARB)==str):
+if(type(VARA)==str or type(VARB)==str):
 	print("string involved")
 if(VARA>VARB):
     print("bigger")
