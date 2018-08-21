0685f5e932041b911803ab545467189018f31f97
chinuteja <chinuteja2008@msitprogram.net>
2018-08-22 00:54:51 +0530

    " -> 5 of 5 passed. pylint: 9.59/10 "

--- cspp1-practice/cspp1 assignment/m14/p2/assignment2.py
+++ cspp1-practice/cspp1 assignment/m14/p2/assignment2.py
@@ -1,20 +1,3 @@
-''' Problem 2 - PlaintextMessage '''
-# For this problem, the graders will use our implementation of the Message class,
-# so don't worry if you did not get the previous parts correct.
-
-# PlaintextMessage is a subclass of Message and has methods to encode a string
-# using a specified shift value. Our class will always create an encoded version
-# of the message, and will have methods for changing the encoding.
-
-# Implement the methods in the class PlaintextMessage according to the specifications in ps6.py.
-# The methods you should fill in are:
-# __init__(self, text, shift): Use the parent class constructor to make your code more concise.
-# The getter method get_shift(self)
-# The getter method get_encrypting_dict(self): This should return a COPY of self.encrypting_dict.
-# The getter method get_message_text_encrypted(self)
-# change_shift(self, shift): Think about what other methods you can use to make this easier.
-
-### Helper code
 '''
 author : teja
 date:21/08/2018
