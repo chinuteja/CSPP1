3a88c294367e5bfe46a7fc8864ad605a89c80662
chinuteja <chinuteja2008@msitprogram.net>
2018-08-22 00:31:07 +0530

    " -> 5 of 5 passed. pylint: 8.33/10 "

--- cspp1-practice/cspp1 assignment/m14/p1/assignment1.py
+++ cspp1-practice/cspp1 assignment/m14/p1/assignment1.py
@@ -1,79 +1,45 @@
-'''Problem 1 - Build the Shift Dictionary and Apply Shift'''
-# The Message class contains methods that could be used to apply a
-# cipher to a string, either to encrypt or to decrypt a message
-# (since for Caesar codes this is the same action).
+'''
+author : teja
+date : 21/08/2018
+'''
+import string
+class Cipher:
+    def __init__(self, value):
+        self.value = value
 
-# In the next two questions, you will fill in the methods of the
-# Message class found in ps6.py according to the specifications in the
-# docstrings. The methods in the Message class already filled in are:
-# __init__(self, text)
-# The getter method get_message_text(self)
-# The getter method get_valid_words(self), notice that this one
-# returns a copy of self.valid_words to prevent someone from mutating the
-# original list.
+    def __len__(self):
+        count = 0
+        for i in self.value:
+            count += 1
+        return count
 
-# In this problem, you will fill in two methods:
-# Fill in the build_shift_dict(self, shift) method of the Message class.
-# Be sure that your dictionary includes both lower and upper case
-# letters, but that the shifted character for a lower case letter and its
-# uppercase version are lower and upper case instances of the
-# same letter. What this means is that if the original letter is "a" and
-# its shifted value is "c", the letter "A" should shift to the letter "C".
+    def shift(self, shift_number):
+        small_alphabet = ""
+        upper_alphabet = ""
+        small_alphabet = "-" + string.ascii_lowercase + string.ascii_lowercase
+        upper_alphabet = "-" + string.ascii_uppercase + string.ascii_uppercase
+        shifted_string = ""
+        a = len(self.value)
+        for i in range(0, a):
+            if self.value[i] in small_alphabet:
+                shifted_string += small_alphabet[small_alphabet.index(self.value[i]) + shift_number]
+            elif self.value[i] in upper_alphabet:
+                shifted_string += upper_alphabet[upper_alphabet.index(self.value[i]) + shift_number]
+            else:
+                shifted_string += self.value[i]
 
-# If you are unfamiliar with the ordering or characters of the English alphabet,
-# we will be following the letter ordering displayed by
-# string.ascii_lowercase and string.ascii_uppercase:
+        return shifted_string
 
-# >>> import string
-# >>> print(string.ascii_lowercase)
-# abcdefghijklmnopqrstuvwxyz
-# >>> print(string.ascii_uppercase)
-# ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-# A reminder from the introduction page - characters such as the space character,
-# commas, periods, exclamation points, etc will not be encrypted by this cipher
-# Basically, all the characters within string.punctuation, plus the space (' ')
-# and all numerical characters (0 - 9) found in string.digits.
-
-# Fill in the apply_shift(self, shift) method of the Message class.
-# You may find it easier to use build_shift_dict(self, shift).
-# Remember that spaces and punctuation should not be changed by the cipher.
-
-# Helper code
-def load_words(file_name):
-    '''
-    file_name (string): the name of the file containing
-    the list of words to load
-
-    Returns: a list of valid words. Words are strings of lowercase letters.
-
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    '''
-
-    # inFile: file
-    in_file = open(file_name, 'r')
-    # line: string
-    line = in_file.readline()
-    # word_list: list of strings
-    word_list = line.split()
-    in_file.close()
-    return word_list
-
-WORDLIST_FILENAME = 'words.txt'
-# Helper code End
-
-
-### Paste your implementation of the Message class here
-        
 
 def main():
     '''
         Function to handle testcases
     '''
-    data = Message(input())
-    data.get_message_text()
-    print(data.apply_shift(int(input())))
+    data_input = input()
+    shift_number = int(input())
+    Cipher_obj = Cipher(data_input)
+    print(Cipher_obj.shift(shift_number))
+    # print(Cipher.shift(data_input, shift_number))
 
 if __name__ == "__main__":
     main()
