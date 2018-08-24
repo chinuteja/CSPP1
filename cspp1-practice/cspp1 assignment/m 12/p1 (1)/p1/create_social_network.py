21facd6cee0acc80602062fff8b58cd02876592e
chinuteja <chinuteja2008@msitprogram.net>
2018-08-24 11:07:49 +0530

    " -> 1 of 3 passed. pylint: 6.19/10 "

--- cspp1-practice/cspp1 assignment/m 12/p1 (1)/p1/create_social_network.py
+++ cspp1-practice/cspp1 assignment/m 12/p1 (1)/p1/create_social_network.py
@@ -37,12 +37,12 @@ def create_social_network(data):
     if 'follows' in data[0]:
     	for i in data:
     		list_1 = i.split("follows")
-    		print(list_1)
+    		#print(list_1)
     		list_2 = list_1[1].split(",")
-    		print(list_2)
+    		#print(list_2)
     		if list_1[0] in dict_1:
     			dict_1[list_1[0]].append(list_2)
-    			print(dict_1)
+    			#print(dict_1)
     		else:
     			dict_1[list_1[0]] = list_2
     			
