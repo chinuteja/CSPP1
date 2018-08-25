dabcc363307aec730a65c8a655f6d1cb1d625a95
chinuteja <chinuteja2008@msitprogram.net>
2018-08-25 11:16:53 +0530

    " -> 2 of 3 passed. pylint: 4.29/10 "

---
+++ cspp1-practice/cspp1 assignment/Module 22/assignment2/clean_input.py
@@ -0,0 +1,22 @@
+'''
+Write a function to clean up a given string by removing the special characters and retain 
+alphabets in both upper and lower case and numbers.
+'''
+
+def clean_string(string):
+    s = ""
+    for i in string:
+        if i in "!@#$%^&*()":
+           i = ""
+           s = s + i
+        else:
+            i = i
+            s = s + i
+    return s
+
+def main():
+    string = input()
+    print(clean_string(string))
+
+if __name__ == '__main__':
+    main()
