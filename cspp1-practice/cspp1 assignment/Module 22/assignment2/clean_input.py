d4bcf3077800bfe2ec67bec5e2c6a5d348bd5bed
chinuteja <chinuteja2008@msitprogram.net>
2018-08-25 11:24:13 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-practice/cspp1 assignment/Module 22/assignment2/clean_input.py
+++ cspp1-practice/cspp1 assignment/Module 22/assignment2/clean_input.py
@@ -4,16 +4,22 @@ date : 25/8/2018
 '''
 
 def clean_string(string):
+	'''
+	function to perform string operation
+	'''
     string_1 = ""
     for i_1 in string:
         if i_1 in ".!@#$%^&*() ":
-           i_1 = ""
-           string_1 = string_1 + i_1
+            i_1 = ""
+            string_1 = string_1 + i_1
         else:
             i_1 = i_1
             string_1 = string_1 + i_1
     return string_1
 def main():
+	'''
+	function to take input
+	'''
     string = input()
     print(clean_string(string))
 
