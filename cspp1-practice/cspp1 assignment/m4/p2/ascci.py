c103f7498366de2452a7ab00b8272f75775164fe
chinuteja <chinuteja2008@msitprogram.net>
2018-12-20 10:33:21 +0530

    " -> 0 of 5 passed. pylint: 6.00/10 "

--- cspp1-practice/cspp1 assignment/m4/p2/ascci.py
+++ cspp1-practice/cspp1 assignment/m4/p2/ascci.py
@@ -1,11 +1,11 @@
-String_1 = str(input("enter a string......  "))
-a_1 = 0
-String_2 = ""
-for i in range(len(String_1)):
-    a_1 = ord((String_1[i:i+1]))
-    print("value of ", a_1)
-    if a_1%2 == 0:
-        String_2 = String_2 + "0"
+Strin_g1 = str(input("enter a string......  "))
+A_a1 = 0
+Strin_g2 = ""
+for i in range(len(Strin_g1)):
+    A_a1 = ord((Strin_g1[i:i+1]))
+    print("value of ", A_a1)
+    if A_a1%2 == 0:
+        Strin_g2 = Strin_g2 + "0"
     else:
-        String_2 = String_2 + "1"
-print("result........", String_2)
+        Strin_g2 = Strin_g2 + "1"
+print("result........", Strin_g2)
