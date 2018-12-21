7d2b470f17390a4f99f70f245e5da07ba9c6572c
chinuteja <chinuteja2008@msitprogram.net>
2018-12-21 11:37:15 +0530

    "cspp1_m10_a1_1: 10/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 2/10 "

--- cspp1-practice/cspp1 assignment/CSPP-1_Question/CSPP-1_Question/sudoku.py
+++ cspp1-practice/cspp1 assignment/CSPP-1_Question/CSPP-1_Question/sudoku.py
@@ -9,14 +9,14 @@ def getcols(suduko, j):
     for k in range(0, 9):
         if(suduko[k][j] != '.'):
             colset.add(suduko[k][j])
-    return colset
+    return colset            
 def getgrid(suduko, i , j):
     gridset = set()
     for row in range(i - (i%3), i + (2 - i%3) + 1):
         for col in range(j - (j%3), j + (2 - j%3) + 1):
             if(suduko[row][col] != '.'):
                 gridset.add(suduko[row][col])
-    return gridset   
+    return gridset
 def main():
     input_1 = input()
     if '.' not in input_1:
@@ -34,9 +34,9 @@ def main():
            for col in range(0, 9):
                if(suduko[row][col] == '.'):
                 result = set(['1','2','3','4','5','6','7','8','9']) - (getrows(suduko, row) | getcols(suduko, col) | getgrid(suduko, row, col))
-                result_1 = ""
+                resultstr = ""
                 for ele in result:
-                    result_1 = result_1 + ele
-                print(resultstr_1)           
+                    resultstr = resultstr + ele
+                print(resultstr)           
 if __name__ == '__main__':
     main()
