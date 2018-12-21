b6e98e64c1c081d2625166cb2bd5ff8b17675d44
chinuteja <chinuteja2008@msitprogram.net>
2018-12-21 10:35:04 +0530

    "cspp1_m10_a1_1: 1/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 6/10 "

---
+++ cspp1-practice/cspp1 assignment/CSPP-1_Question/CSPP-1_Question/sudoku.py
@@ -0,0 +1,24 @@
+def check_suduko(suduko):
+    count = 0
+    for row in suduko:
+        # print(row)
+        for i in row:
+            if i == ".":
+                count = count + 1
+            
+    if count == 0:
+        return "Given sudoku is solved"
+def main():
+    suduko = []
+    input_1 = str(input())
+    i = 0
+    j = 0
+    while i< len(input_1):
+        suduko.insert(j,list((input_1[i:i+9])))
+        # print("sujo",suduko)
+        i = i + 9
+        j = j + 1
+    print(check_suduko(suduko))
+    # print(suduko)
+if __name__ == "__main__":
+    main()
