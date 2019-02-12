0de6360129de0d7c19f16a1c5af4cfd25f6d7510
chinuteja <chinuteja2008@msitprogram.net>
2019-02-12 17:10:06 +0530

    "cspp2_m10_a1_1: 2/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- CSPP2Remedial Exam4(1)/CSPP2Remedial Exam4/Game.java
+++ CSPP2Remedial Exam4(1)/CSPP2Remedial Exam4/Game.java
@@ -56,8 +56,6 @@ public class Game {
 	    	if (p[i].getScore() > temp) {
 	    		temp = p[i].getScore();
 	    		j = i;
-	    		System.out.println("name " +p[i].getName());
-	    		System.out.println(temp);
 	    	}
 	    }
 	    System.out.println(p[j].getName());
